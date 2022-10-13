93af70d3fa1250ca7458f8e3a059dcac845baa5e
Ahmed Hanafy <ahmedhanafyr99@gmail.com>
2022-10-12 18:57:21 -0700

    Commit to be able to merge

---
+++ client/src/Components/Overview/OverviewApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductInfo from './ProductInfo.jsx'
+
+const OverviewApp = () => {
+  return (
+    <div>
+      <ProductInfo />
+    </div>
+  )
+}
+
+export default OverviewApp;
