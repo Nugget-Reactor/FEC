dd4ae2ac39ac12ed66bf8da7978862b19afde168
Ahmed Hanafy <ahmedhanafyr99@gmail.com>
2022-10-13 11:34:01 -0700

    Retrieving required data for Overview

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,31 +1,18 @@
-<<<<<<< HEAD
 import React, {useState, useEffect} from 'react';
 import OverviewApp from '../Components/Overview/OverviewApp.jsx';
-import axios from 'axios';
-import Ratings from './R&R/ratings.jsx';
-=======
-import React, { useState, useEffect } from 'react';
+import Ratings from './R&R/Ratings.jsx';
 import axios from 'axios';
 import QnA from './QnA/QnA.jsx';
->>>>>>> main
 
 const App = () => {
 
 
   return (
-<<<<<<< HEAD
     <div>
       <OverviewApp />
       <Ratings />
-    </div>
-=======
-    <>
-      <div>
-        Hello World
-      </div>
       <QnA />
-    </>
->>>>>>> main
+    </div>
   )
 }
 
--- client/src/Components/Overview/ProductInfo.jsx
+++ client/src/Components/Overview/ProductInfo.jsx
@@ -4,23 +4,34 @@ import axios from 'axios';
 const {useState, useEffect} = React
 
 const ProductInfo = () => {
-  const [product, setProduct] = useState([]);
-  const [productReviews, setProductReviews] = ([]);
+  const [product, setProduct] = useState({});
+  const [productStyles, setProductStyles] = useState([]);
+  const [currentStyle, setCurrentStyle] = useState({})
 
   useEffect(() => {
-    axios.get('./products')
-      .then(res => setProduct(res.data[0]))
+    axios.get('/products/40344')
+      .then(res => setProduct(res.data))
       .catch(err => console.error(err))
   }, [])
 
+  useEffect(() => {
+    if (product.id) {
+      axios.get(`/products/${product.id}/styles`)
+      .then(res => setProductStyles(res.data.results))
+      .catch(err => console.error(err))
+    }
+  }, [product])
+
+
+
   return (
-    <div>
-      <ul>
-        <li>{product.name}</li>
-        <li>{product.slogan}</li>
-        <li>{product.description}</li>
-        <li>{product.default_price}</li>
-      </ul>
+    <div className="product-info">
+      <div>Category</div>
+      <div>{product.category}</div>
+      <div>{product.name}</div>
+      {productStyles.length > 0 &&
+        <div>{productStyles[0].original_price}</div>
+      }
     </div>
   )
 }
