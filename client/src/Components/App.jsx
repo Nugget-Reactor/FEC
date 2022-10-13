8b60b4d6ea40062cda62150bb6db89fbcd52fa5f
Ahmed Hanafy <ahmedhanafyr99@gmail.com>
2022-10-13 09:31:45 -0700

    Commit to merge

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
   return (
     <div>
       <OverviewApp />
-      Hello World
       <Ratings />
     </div>
   )
--- client/src/Components/Overview/ProductInfo.jsx
+++ client/src/Components/Overview/ProductInfo.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-const {useState, useEffect} = 'react'
+import React from 'react';
+import axios from 'axios';
+
+const {useState, useEffect} = React
 
 const ProductInfo = () => {
+  const [product, setProduct] = useState([]);
+  const [productReviews, setProductReviews] = ([]);
+
+  useEffect(() => {
+    axios.get('./products')
+      .then(res => setProduct(res.data[0]))
+      .catch(err => console.error(err))
+  }, [])
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <ul>
+        <li>{product.name}</li>
+        <li>{product.slogan}</li>
+        <li>{product.description}</li>
+        <li>{product.default_price}</li>
+      </ul>
+    </div>
   )
 }
 
