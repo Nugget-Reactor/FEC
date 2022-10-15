4fd998528b3f9e0a2d611f72f8be4718dd45dba5
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-14 18:05:45 -0700

    tidied up code in Related Items

--- client/src/Components/RelatedItems/RelatedItem.jsx
+++ client/src/Components/RelatedItems/RelatedItem.jsx
@@ -6,7 +6,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 const RelatedItem = ({relatedItem, handleProductChange}) => {
-  // console.log("related item in relatedItem line 7", relatedItem);
+  // console.log("related item in relatedItem line 7", relatedItem); //need this to access characteristics later
   const [currentStyle, setCurrentStyle] = useState({});
   const [currentPhotoURL, setCurrentPhotoURL] = useState('');
   const [regPrice, setRegPrice] = useState('');
@@ -40,12 +40,12 @@ const RelatedItem = ({relatedItem, handleProductChange}) => {
     }
   }, [currentStyle]);
 
-  // need action button to look better and be functional => Compare modal
-  // also if no reviews, no star div
+  // need action button to look better/be more accessible, and be functional => Compare modal
+  // also if no reviews, this div should be hidden
   return (
     <RelatedItemListItem onClick={(event) => handleProductChange(relatedItem.id)}>
       <RelatedImageDiv><RelatedDefaultImage src={currentPhotoURL}/>
-        <ActionButtonRelatedIcon><ActionButtonRelated></ActionButtonRelated></ActionButtonRelatedIcon></RelatedImageDiv>
+    <ActionButtonRelated></ActionButtonRelated></RelatedImageDiv>
       <h5>{relatedItem.category}</h5>
       <h4>{relatedItem.name}</h4>
       <Pricing salePrice={salePrice} regPrice={regPrice} strikePrice={strikeRegPrice} />
@@ -56,6 +56,7 @@ const RelatedItem = ({relatedItem, handleProductChange}) => {
 
 export default RelatedItem;
 
+/** function for handling strikethrough proicing/ sale pricing/ regular pricing */
 const Pricing = ({salePrice, regPrice, strikePrice}) => {
   if (salePrice) {
     return <SaleAndStrikeBlock><SalePricing>${salePrice}</SalePricing><StrikePricing>${strikePrice}</StrikePricing></SaleAndStrikeBlock>
@@ -63,6 +64,14 @@ const Pricing = ({salePrice, regPrice, strikePrice}) => {
     return <div>${regPrice}</div>
   }
 };
+
+const RelatedItemListItem = styled.li`
+  display: inline-block;
+  border-radius: 3px;
+  margin: 5px;
+  border: 2px solid black;
+  padding: 5px;
+`
 
 const RelatedImageDiv = styled.div`
   position:relative;
@@ -72,6 +81,12 @@ const RelatedImageDiv = styled.div`
   width: 250px;
   word-wrap: normal;
 `
+const RelatedDefaultImage = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`
+
 const ActionButtonRelated = styled.button`
   position:absolute;
   right: 6px;
@@ -85,46 +100,18 @@ const ActionButtonRelated = styled.button`
     content: "\\f005";
   }
 `
-const ActionButtonRelatedIcon = styled.button`
-  position:absolute;
-  font-size: 41px;
-  right: 0;
-  top: 0;
-  background: none;
-  border: none;
-  color: #000;
-  &::after {
-    font-family: FontAwesome;
-    content: "\\f005";
-  }
-`
-
-const RelatedDefaultImage = styled.img`
-height: 100%;
-width: 100%;
-object-fit: cover;
-`
-
-const RelatedItemListItem = styled.li`
-display: inline-block;
-border-radius: 3px;
-margin: 5px;
-border: 2px solid black;
-padding: 5px;
-`
 
 const SaleAndStrikeBlock = styled.div`
-// display: inline;
-display: inline-block;
+  display: inline-block;
 `
 
 const SalePricing = styled.div`
-color: red;
-margin-right: 5px;
-display: inline-block;
+  color: red;
+  margin-right: 5px;
+  display: inline-block;
 `
 
 const StrikePricing = styled.div`
-text-decoration: line-through;
-display: inline-block;
+  text-decoration: line-through;
+  display: inline-block;
 `
--- client/src/Components/RelatedItems/RelatedItems.jsx
+++ client/src/Components/RelatedItems/RelatedItems.jsx
@@ -3,19 +3,6 @@ import axios from 'axios';
 import RelatedItem from './RelatedItem.jsx';
 import styled from 'styled-components';
 
-
-var testProduct = {
-  "id": 40344,
-  "campus": "hr-rfp",
-  "name": "Camo Onesie",
-  "slogan": "Blend in to your crowd",
-  "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
-  "category": "Jackets",
-  "default_price": "140.00",
-  "created_at": "2021-08-13T14:38:44.509Z",
-  "updated_at": "2021-08-13T14:38:44.509Z"
-};
-
 const RelatedItems = ({product, handleProductChange}) => {
   const [characteristics, setCharacteristics] = useState([]);
   const [relatedItems, setRelatedItems] = useState([]);
@@ -23,6 +10,7 @@ const RelatedItems = ({product, handleProductChange}) => {
   const [relatedImages, setRelatedImages] = useState([]);
 
   useEffect(() => {
+    /**to get related item **/
     if (product.id) {
       axios.get(`/products/${product.id}/related`)
       .then((results) => {
@@ -30,14 +18,13 @@ const RelatedItems = ({product, handleProductChange}) => {
       })
       .catch((err) => console.log('error', err));
 
-      //to do for modal
+      //to do for modal to get characteristics of current item from metadata if not passed down as props
       // axios.get(`/reviews/meta?product_id=${product.id}`) //actually want currentItem.id/related
       // .then((results) => {
       //   console.log('characteristics in Related Items', results);
       //   setRelatedItems(results.data);  //I want this to be an array of objects with all of the properties I want
       // })
       // .catch((err) => console.log('error', err));
-
     }
   }, [product]);
 
@@ -51,16 +38,15 @@ const RelatedItems = ({product, handleProductChange}) => {
         </RelatedItemsList>
       </div>
   )
-
 }
 
 export default RelatedItems;
 
 const RelatedItemsList = styled.ul`
-display: inline-block;
-border-radius: 3px;
-border: 2px solid black;
-padding: 0.5rem 0;
+  display: inline-block;
+  border-radius: 3px;
+  border: 2px solid black;
+  padding: 0.5rem 0;
 `
 
 
