0499402add38bdc3a2572b26d7b26f6e666dabf7
Curtis Wang <curtis.wang10@gmail.com>
2022-10-17 16:01:49 -0700

    trying to figure out react testing library and jest

--- client/src/Specs/QnA.spec.js
+++ client/src/Specs/QnA.spec.js
@@ -1,18 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import QnA from '../Components/QnA/QnA.jsx';
 
-// describe('QnA Feature', () => {
-//   test('renders QnA feature component', () => {
-//     render(<QnA />);
+describe('QnA Feature', () => {
+  test('renders QnA feature component', () => {
+    render(<QnA />);
 
-//     screen.debug();
-//   });
-// });
-
-describe('true is truthy', () => {
-  test('true is truthy', () => {
-    expect(true).toBe(true);
+    screen.debug();
   });
 });
+
+// describe('true is truthy', () => {
+//   test('true is truthy', () => {
+//     expect(true).toBe(true);
+//   });
+// });
