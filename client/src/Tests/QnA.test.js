7d1f7d65172e2019112036fc7d93483cb7fe46d9
Curtis Wang <curtis.wang10@gmail.com>
2022-10-17 19:12:11 -0700

    still messing around with testing suite and cannot figure it out

---
+++ client/src/Tests/QnA.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from '../Components/App.jsx';
+
+// describe('QnA Feature', () => {
+//   test('renders App feature component', () => {
+//     render(<App />);
+//   });
+// });
+
+describe('true is truthy', () => {
+  test('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+});
