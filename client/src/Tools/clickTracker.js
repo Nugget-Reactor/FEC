3a1d27b585bb7599199e0e1bdd7522579ff628f7
Curtis Wang <curtis.wang10@gmail.com>
2022-10-25 15:59:49 -0700

    still adjusting clickTracker

--- client/src/Tools/clickTracker.js
+++ client/src/Tools/clickTracker.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export const useClickTracker = () => {
+export const useClickTracker = (component) => {
   const [element, setElement] = useState('');
   const [time, setTime] = useState('');
   const [widget, setWidget] = useState('');
 
   useEffect(() => {
-
+    // setWidget(clickedWidget);
+    // setElement(e.target.nodeName);
+    // setTime(new Date());
   });
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log('clicked', e.target);
+    console.log('clicked', e.target, component);
     let interaction = {};
     interaction.element = element;
     interaction.widget = widget;
