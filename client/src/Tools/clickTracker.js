f4fe89833ed742ecd6c352d84885a79489087322
Curtis Wang <curtis.wang10@gmail.com>
2022-10-25 18:46:53 -0700

    removed unnecessary commented code from clickTracker

--- client/src/Tools/clickTracker.js
+++ client/src/Tools/clickTracker.js
@@ -1,30 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-/* export const useClickTracker = () => {
-  const [element, setElement] = useState('');
-  const [time, setTime] = useState('');
-  const [widget, setWidget] = useState('');
-
-  useEffect(() => {
-    // setWidget(clickedWidget);
-    // setElement(e.target.nodeName);
-    // setTime(new Date());
-  });
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    console.log('clicked', e.target, component);
-    let interaction = {};
-    interaction.element = element;
-    interaction.widget = widget;
-    interaction.time = time;
-    axios.post('/interactions', interaction)
-      .then(results => console.log('Successfully logged interaction'))
-      .catch(err => console.log('Error logging interaction'));
-  };
-}; */
-
 export const TrackerContext = React.createContext();
 export const TrackerFunction = (element, widget) => {
   let interaction = {
