1c1c3df30182eaedbbd2514c0032205a297043b7
jonahchoi <cjonah227@gmail.com>
2022-10-28 09:23:42 -0700

    Changed overflow back to auto

--- client/src/spec/Reviews.test.js
+++ client/src/spec/Reviews.test.js
@@ -4,28 +4,32 @@
 import React from 'react';
 import { screen, render, cleanup, waitFor, fireEvent } from '@testing-library/react'
 import axiosMock from "axios";
+
 import Reviews from '../Components/R&R/Reviews.jsx';
+import Breakdown from '../Components/R&R/Breakdown.jsx'
+import CharacteristicInput from '../Components/R&R/CharacteristicInput.jsx'
+import CharacterTile from '../Components/R&R/CharacterTile.jsx'
+import List from '../Components/R&R/List.jsx'
+import ModalForm from '../Components/R&R/ModalForm.jsx'
+import RatingFilter from '../Components/R&R/RatingFilter.jsx'
+import Tile from '../Components/R&R/Tile.jsx'
 
 import sampleReviews from './sampleData/Reviews/sampleReviews.js';
 import sampleMetadata from './sampleData/Reviews/sampleMetadata.js';
 
-describe('Reviews component testing', () => {
-  /*  beforeAll(() => {
-     render(<Reviews />)
-   }); */
-  /* beforeAll(() => {
-    render(<Reviews />)
-  }); */
+describe('Reviews components testing', () => {
+
   afterEach(cleanup);
 
+  const productID = 12345;
+
   it('Reviews should render', async () => {
-    const productID = 12345;
 
     axiosMock.get.mockImplementation((url) => {
       if (url === `/reviews?product_id=${productID}&sort=relevant&count=50`) {
         return Promise.resolve(sampleReviews);
       }
-    })
+    });
     const { getByTestId, getByText } = render(<Reviews productID={productID} productName="Product1" currentMeta={sampleMetadata} />)
 
     expect(screen.getByText('Ratings & Reviews')).toBeInTheDocument();
@@ -34,5 +38,9 @@ describe('Reviews component testing', () => {
     const reviewTitle = await waitFor(() => getByTestId('reviewTitle'));
 
     expect(reviewTitle).toHaveTextContent('0 reviews');
+  });
+
+  it('Breakdown should render', async () => {
+
   })
 })
