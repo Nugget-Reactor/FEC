041b209cbf17a69798ea051347b64a0657aed26f
Curtis Wang <curtis.wang10@gmail.com>
2022-10-29 12:32:57 -0700

    seeing if box shadow adds to components at all, only adds to buttons nicely

--- client/src/Components/QnA/QuestionsList.jsx
+++ client/src/Components/QnA/QuestionsList.jsx
@@ -71,6 +71,7 @@ const AddQButton = styled.button`
   font-size: 1rem;
   margin: 10px;
   cursor: pointer;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 `;
 
 const MoreQuestions = styled.button`
@@ -80,4 +81,5 @@ const MoreQuestions = styled.button`
   font-size: 1rem;
   margin: 10px;
   cursor: pointer;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 `;
